6918e2a3aee9034601816029707d2bdc5983f762
Yuval Schwartz <yuvsc@post.bgu.ac.il>
2024-02-15 11:02:37 +0200

    Finishing model_evaluation module.

--- pipeline/main.py
+++ pipeline/main.py
@@ -11,16 +11,19 @@ from sklearn.linear_model import LogisticRegression
 import xgboost as xgb
 from sklearn.ensemble import RandomForestClassifier
 
-#editor_fold
-seed=1
-reg_alpha_param=0.2
+
+# editor_fold
+seed = 1
+reg_alpha_param = 0.2
+
+
 # Main function of the pred-ICU Pipeline
 def main(model):
     # Setup logging
     setup_logging()
 
     # Create objects
-    data_handler = DataHandler(file_path=('data/training_v2.csv'))
+    data_handler = DataHandler(file_path=os.path.abspath('..\\data\\training_v2.csv'))
     preprocessing = Preprocessing(scaler=MinMaxScaler())
     model_handler = ModelHandler(model=model)
 
@@ -38,34 +41,34 @@ def main(model):
 
 if __name__ == '__main__':
     # run main on a list of models
-    models = []
-    models.append(LogisticRegression(penalty='l1', solver='saga', max_iter=1000,random_state=seed))
-    models.append(xgb.XGBClassifier(random_state=seed,reg_alpha=reg_alpha_param))
-    models.append(RandomForestClassifier(random_state=seed))
+    # models = []
+    # models.append(LogisticRegression(penalty='l1', solver='saga', max_iter=1000, random_state=seed))
+    # models.append(xgb.XGBClassifier(random_state=seed, reg_alpha=reg_alpha_param))
+    # models.append(RandomForestClassifier(random_state=seed))
 
-    list_of_results_file_paths = [main(model) for model in models]
+    # list_of_results_file_paths = [main(model) for model in models]
+    logistic_regression_results_file_path = os.path.abspath('..\\predictions\\predictions_2024-02-14_02-03-55.json')
+    xgb_results_file_path = os.path.abspath('..\\predictions\\predictions_2024-02-14_01-19-05.json')
+    random_forest_results_file_path = os.path.abspath('..\\predictions\\predictions_2024-02-14_01-39-00.json')
+    list_of_results_file_paths = []
+    list_of_results_file_paths.append(logistic_regression_results_file_path)
+    list_of_results_file_paths.append(xgb_results_file_path)
+    list_of_results_file_paths.append(random_forest_results_file_path)
 
     # Create ModelEvaluation object
-    cutoffs = [0.01, 0.05, 0.1, 0.2]
-    model_evaluation = ModelEvaluation(json_files=list_of_results_file_paths, cutoffs=cutoffs)
+    model_evaluation = ModelEvaluation(json_files=list_of_results_file_paths)
 
     # Plot ROC curves
-    # model_evaluation.plot_roc_curves()
+    # model_evaluation.plot_roc_curves(including_apache=True, including_cutoffs=False, including_confidence_intervals=False)
 
     # Plot Precision-Recall curves
-    # model_evaluation.plot_precision_recall_curves(including_confidence_intervals=False)
+    # model_evaluation.plot_precision_recall_curves(including_apache=True, including_cutoffs=False, including_confidence_intervals=False)
 
     # Plot Sensitivity-Percent-Positives curves
-    # model_evaluation.plot_sensitivity_percent_positives_curves(including_confidence_intervals=False)
+    model_evaluation.plot_sensitivity_percent_positives_curves(including_apache=True, including_cutoffs=False, including_confidence_intervals=False)
 
     # Plot Precision-Percent-Positives curves
-    # model_evaluation.plot_precision_percent_positives_curves(including_confidence_intervals=False)
+    # model_evaluation.plot_precision_percent_positives_curves(including_apache=True, including_cutoffs=False, including_confidence_intervals=False)
 
     # Generate predictions .csv files for calibrations plots
-    model_evaluation.generate_predictions_files()
-
-    # Calculate metrics
-    # list_of_thresholds = [0.7,0.8,0.85,0.9,0.95]
-    # metrics = model_evaluation.calculate_metrics_with_bootstrap(list_of_thresholds, n_bootstrap=100)
-    # print(metrics)
-    # model_evaluation.plot_metrics_with_ci(metrics)
+    # model_evaluation.generate_predictions_files()
